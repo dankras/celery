100a7463e5a8ff3c6ff3f39cf2882a4a91f81e00
Dan Kras <dan.kras@gmail.com>
2020-10-02 11:15:37 -0500

    fix vine req + add scheduler

--- tasks.py
+++ tasks.py
@@ -1,9 +1,19 @@
 import os
 from celery import Celery
 
-app = Celery('tasks', broker=os.getenv("CELERY_BROKER_URL"))
+app = Celery('tasks', broker=os.getenv("CELERY_BROKER_URL", "redis://localhost"))
+
+
+app.conf.beat_schedule = {
+    "add_2_plus_2_every_minute": {
+        "task": "tasks.add",
+        "schedule": crontab(),
+        'args': (2, 2)
+    },
+}
 
 
 @app.task
 def add(x, y):
+	print(x + y)
     return x + y
